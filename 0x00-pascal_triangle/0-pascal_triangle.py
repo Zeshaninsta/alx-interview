affeec0acb04194abf06509c1e2ced819b1f6f63
zeshaninsta <zeshaninsta@gmail.com>
2024-06-28 05:05:29 +0300

    pascal triangle added

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""0-pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """
+    Returns:
+        Pascal Triangle in a list form
+    """
+    triangle = []
+    if n <= 0:
+        return triangle
+    triangle = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(triangle[i - 1]) - 1):
+            curr = triangle[i - 1]
+            temp.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
+        temp.append(1)
+        triangle.append(temp)
+    return triangle
